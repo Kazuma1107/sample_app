71b31fce2d923f62b779a7d0530bb07c8c011223
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-01-08 07:35:03 +0000

    rails c の実装スタート

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,8 @@
 class List < ApplicationRecord
   has_one_attached :image
+  
+  validates :title, presence: true
+  validates :body,  presence: true
+  #コメントアウト　rails　ｃを行う際に、画像を扱う場合は複雑になるため。
+ # validates :image, presence: true
 end
